ab6538f425bf79e9716bbab70a22f5de82fdc7c8
ravisachdeva83 <101047485+ravisachdeva83@users.noreply.github.com>
2022-04-20 19:51:40 +0530

    Added Code File Demonstrating Use of 2D Array

---
+++ 2D Array.cpp
@@ -0,0 +1,56 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int studentMarks[3][3]={10,20,30,40,50,60,70,80,90};
+        
+        
+    for (int i=0;i<=2;i++)
+    {
+        for (int j=0;j<=2;j++)
+        {
+            cout<<studentMarks[i][j];
+            cout<<"\n";
+            
+        }
+    }
+    
+    cout<<"*********\n";
+    
+    for (int i=0;i<=2;i++)
+    {
+        for (int j=0;j<=2;j++)
+        {
+            
+            cout<<*(studentMarks[i]+j);
+            cout<<"\n";
+            
+        }
+    }
+    
+    cout<<"*********\n";
+    
+    for (int i=0;i<=2;i++)
+    {
+        for (int j=0;j<=2;j++)
+        {
+            
+            cout<<*(*(studentMarks+i)+j);
+            cout<<"\n";
+        }
+    }
+    
+    
+
+    return 0;
+}
